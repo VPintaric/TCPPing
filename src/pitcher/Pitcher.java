e108a8b5555431974b14ed40b0963061a39d46c8
Vedran Pintaric <vedran.pintaric@fer.hr>
2017-01-08 01:37:10 +0100

    Some upgrades to Pitcher, need to add packet numbering

--- src/pitcher/Pitcher.java
+++ src/pitcher/Pitcher.java
@@ -4,16 +4,24 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class Pitcher {
+	private static final long MAX_ITERATION_TIME = 1000L; // a bit more than a second
+	private static final long SERVER_FINAL_TIME_OUT = 5000L;
+	
 	Random rng;
 	int port, mps, size;
 	String host_name;
+	Thread thisThread;
 	
 	/**
+	 * NOTE: In case of a slow connection real messages per second rate could be
+	 * lower than the "mps" parameter.
 	 * 
 	 * @param port number of "Catcher" this "Pitcher" will try to connect to
 	 * @param mps messages sent per second to "Catcher"
@@ -21,6 +29,7 @@ public class Pitcher {
 	 * @param host name of "Catcher"
 	 */
 	public Pitcher(int port, int mps, int size, String host){
+		thisThread = Thread.currentThread();
 		this.host_name = host;
 		this.port = port;
 		this.mps = mps;
@@ -34,12 +43,15 @@ public class Pitcher {
 	 */
 	private byte[] generateRandomData(int size){
 		byte[] data = new byte[size];
-		for(int i = 0; i < size; i++)
-			data[i] = 'a';
+		rng.nextBytes(data);
 		return data;
-		//byte[] data = new byte[size];
-		//rng.nextBytes(data);
-		//return data;
+	}
+
+	private String getTimeString(double time, boolean timedOut) {
+		if(timedOut)
+			return "timed out";
+		else
+			return time + " ms";
 	}
 	
 	/**
@@ -48,29 +60,44 @@ public class Pitcher {
 	 * @throws IOException
 	 */
 	public void run() throws Exception{
+		@SuppressWarnings("resource")
 		Socket s = new Socket(host_name, port);
 		DataOutputStream toServer = new DataOutputStream(s.getOutputStream());
 		DataInputStream fromServer = new DataInputStream(s.getInputStream());
 		
 		long sentMessages = 0L;
 		long waitTime = 1000L / mps;
+		double realMps = mps;
+		boolean socketTimeOut = false, serverTimeOut = false;
 		while(true){
 			double avgPCtime = 0., avgCPtime = 0., avgPCPtime = 0.;
 			long  maxPCtime = 0L, maxCPtime = 0L, maxPCPtime = 0L;
 			
-			for(int i = 0; i < mps; i++){
+			long startTimeThisIter = System.currentTimeMillis();
+			long messagesThisIter = 0L;
+			
+			socketTimeOut = false;
+			while(messagesThisIter < mps && 
+					System.currentTimeMillis() - startTimeThisIter < MAX_ITERATION_TIME){
+				
 				// Send
+				long timeOfSending = System.currentTimeMillis();
 				toServer.writeInt(size);
-				byte[] data = generateRandomData(size - Integer.SIZE / Byte.SIZE);
+				byte[] data = generateRandomData(size - Integer.BYTES);
 				toServer.write(data);
 				
-				long timeOfSending = System.currentTimeMillis();
-				
-				sentMessages++;
+				messagesThisIter++;
 				
 				// Receive
-				long timeOfRecvC = fromServer.readLong();
-				fromServer.read(data, 0, size - Long.SIZE / Byte.SIZE);
+				s.setSoTimeout((int) (MAX_ITERATION_TIME - (System.currentTimeMillis() - startTimeThisIter)));
+				long timeOfRecvC;
+				try{
+					timeOfRecvC = fromServer.readLong();
+					fromServer.readFully(data, 0, size - Long.BYTES);
+				} catch(SocketTimeoutException exc){
+					socketTimeOut = true;
+					break;
+				}
 				
 				long timeOfRecvP = System.currentTimeMillis();
 				
@@ -86,29 +113,44 @@ public class Pitcher {
 				maxCPtime = Math.max(maxCPtime, CPtime);
 				maxPCPtime = Math.max(maxPCPtime, PCPtime);
 				
-				Thread.sleep(waitTime - PCPtime);
+				Thread.sleep(Math.max(waitTime - PCPtime, 0L));
+			}
+			serverTimeOut = messagesThisIter == 1 && socketTimeOut;
+			sentMessages += messagesThisIter;
+			long thisIterDuration = System.currentTimeMillis() - startTimeThisIter;
+			realMps = (double) messagesThisIter / thisIterDuration * 1000;
+			
+			if(!serverTimeOut){
+				avgPCtime /= messagesThisIter;
+				avgCPtime /= messagesThisIter;
+				avgPCPtime /= messagesThisIter;
 			}
 			
-			avgPCtime /= mps;
-			avgCPtime /= mps;
-			avgPCPtime /= mps;
-			
-			Calendar calendar = Calendar.getInstance();
-			System.out.println("Time: " + calendar.get(Calendar.HOUR_OF_DAY) + ":"
-											+ calendar.get(Calendar.MINUTE) + ":"
-											+ calendar.get(Calendar.SECOND));
+			// Printing out the needed information
+			Date date = new Date();
+			SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+			System.out.println("Time: " + format.format(date));
+
 			System.out.println("Total messages sent: " + sentMessages);
-			System.out.println("Messages per second: " + mps);
+			System.out.println("Messages per second: " + realMps);
 			
-			System.out.println("Pitcher->Catcher average time: " + avgPCtime);
-			System.out.println("Catcher->Pitcher average time: " + avgCPtime);
-			System.out.println("Pitcher->Catcher->Pitcher average time: " + avgPCPtime);
+			System.out.println("Pitcher->Catcher average time: " + getTimeString(avgPCtime, serverTimeOut));
+			System.out.println("Catcher->Pitcher average time: " + getTimeString(avgCPtime, serverTimeOut));
+			System.out.println("Pitcher->Catcher->Pitcher average time: " + getTimeString(avgPCPtime, serverTimeOut));
 			
-			System.out.println("Pitcher->Catcher max time: " + maxPCtime);
-			System.out.println("Catcher->Pitcher max time: " + maxCPtime);
-			System.out.println("Pitcher->Catcher->Pitcher max time: " + maxPCPtime);
+			System.out.println("Pitcher->Catcher max time: " + getTimeString(maxPCtime, serverTimeOut));
+			System.out.println("Catcher->Pitcher max time: " + getTimeString(maxCPtime, serverTimeOut));
+			System.out.println("Pitcher->Catcher->Pitcher max time: " + getTimeString(maxPCPtime, serverTimeOut));
 			
 			System.out.println("-----------------------------------");
+			
+			// TODO this kind of works, but implement packet numbers, you moron
+			if(socketTimeOut){
+				while(fromServer.available() < size){
+					Thread.sleep(50);
+				}
+				fromServer.skipBytes(fromServer.available());
+			}
 		}
 	}
 }
